88182140ccf72ef2a4b2604c534898767c555959
William F. Broderick <billbrod@gmail.com>
2024-05-09 16:47:09 -0400

    add emacs to github action

--- pelicanconf.py
+++ pelicanconf.py
@@ -66,7 +66,7 @@ PLUGIN_PATHS = ['pelican-plugins', 'liquid_tags', 'extra_plugins']
 PLUGINS = ['org_reader', 'pelican-bibtex', 'simple_footnotes']
 PUBLICATIONS_SRC = 'content/docs/pubs.bib'
 
-ORG_READER_EMACS_LOCATION = '/usr/bin/emacs'
+ORG_READER_EMACS_LOCATION = 'emacs'
 # solution from https://emacs.stackexchange.com/a/32818
 ORG_READER_EMACS_SETTINGS = os.path.abspath('lisp/config.el')
 ORG_READER_BACKEND = "'pelican-html"
